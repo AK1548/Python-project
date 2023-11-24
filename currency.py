3e740959c7b91a5d972d12a2fd2bf2ba93342655
AK1548 <kamathaditya6699@gmail.com>
2023-11-24 21:17:32 +0530

    Update currency.py

--- currency.py
+++ currency.py
@@ -1,2 +1,4 @@
 import streamlit as st
 import requests
+
+st.markdown('<iframe src="https://lottie.host/embed/db1e1472-53bf-4d21-9383-df234ff6d6da/ZdRsjnzH1u.json"></iframe>',unsafe_allow_html=True)
