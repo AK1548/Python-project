53a6ca54c12bb881115f0269cf9762341b49111d
AK1548 <kamathaditya6699@gmail.com>
2023-11-27 14:42:21 +0530

    Update currency.py

--- currency.py
+++ currency.py
@@ -8,10 +8,16 @@ with col1:
     curr1 = st.selectbox('Currency1',['EUR','USD','GBP'])
 with col2:
     if curr1 =='EUR':
-        st.markdown('<iframe src="https://lottie.host/embed/d28e1f50-2898-4747-a02d-01439edbc13c/mfCpMXpv7t.json"></iframe>',unsafe_allow_html=True)
+        one_two = 0.91
     elif curr1 =='USD':
         st.markdown('<iframe src="https://lottie.host/embed/d28e1f50-2898-4747-a02d-01439edbc13c/mfCpMXpv7t.json"></iframe>',unsafe_allow_html=True)
     else:
         st.markdown('<iframe src="https://lottie.host/embed/d28e1f50-2898-4747-a02d-01439edbc13c/mfCpMXpv7t.json"></iframe>',unsafe_allow_html=True)
 with col3:
     curr2 = st.selectbox('Currency 2',['EUR','USD','GBP'])
+
+with col1:
+    amount=st.number_input(curr1)
+with col2:
+    converted=amount*one_two
+    st.success(converted)
