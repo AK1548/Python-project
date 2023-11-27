2d99ba9e0a374ee996fb3f92f62069afc76c6ccd
AK1548 <kamathaditya6699@gmail.com>
2023-11-27 20:55:24 +0530

    Squashed commit of the following:
    commit cabde8ec302934e6283dd72ab74533f059bd8c26
    Author: AK1548 <kamathaditya6699@gmail.com>
    Date:   Mon Nov 27 20:53:10 2023 +0530
    
        Update currency.py
    
    commit 42dfa0709eaace7f227a051d992d1347bbdb587d
    Author: AK1548 <kamathaditya6699@gmail.com>
    Date:   Mon Nov 27 20:11:59 2023 +0530
    
        Update currency.py
    
    commit bcb6c8e273f3be60064b5df89cbbf8bebc8aabe5
    Author: AK1548 <kamathaditya6699@gmail.com>
    Date:   Mon Nov 27 14:43:38 2023 +0530
    
        Update currency.py
    
    commit 53a6ca54c12bb881115f0269cf9762341b49111d
    Author: AK1548 <kamathaditya6699@gmail.com>
    Date:   Mon Nov 27 14:42:21 2023 +0530
    
        Update currency.py
    
    commit 99aa1f1f8c07a55b09039e551ab56e1c967b4555
    Author: AK1548 <kamathaditya6699@gmail.com>
    Date:   Mon Nov 27 14:07:38 2023 +0530
    
        Update currency.py

--- currency.py
+++ currency.py
@@ -1,18 +1,43 @@
 import streamlit as st
-import requests
-import api
-
-st.markdown('<iframe src="https://lottie.host/embed/db1e1472-53bf-4d21-9383-df234ff6d6da/ZdRsjnzH1u.json"></iframe>',unsafe_allow_html=True)
+st.markdown('<h1 style=font-size:40px; >Unit & Currency Converter</h1>', unsafe_allow_html=True)
+centered_markdown = """
+<div style="display: flex; justify-content: center; align-items: center; height: 30vh;">
+    <iframe src="https://lottie.host/embed/166bec96-faad-4ed8-aa2f-8f393e6563c7/MotO6059jJ.json" width="200" height="200"></iframe>
+</div>
+"""
+st.markdown(centered_markdown, unsafe_allow_html=True)
+st.markdown('<h1>Currency</h1>', unsafe_allow_html=True)
 col1,col2,col3 = st.columns(3)
 with col1:
-    curr1 = st.selectbox('Currency1',['EUR','USD','GBP'])
+    curr1 = st.selectbox('Currency1',['EUR','USD','GBP','INR'])
 with col2:
-    if curr1 =='EUR':
-        st.markdown('<iframe src="https://lottie.host/embed/d28e1f50-2898-4747-a02d-01439edbc13c/mfCpMXpv7t.json"></iframe>',unsafe_allow_html=True)
-    elif curr1 =='USD':
-        st.markdown('<iframe src="https://lottie.host/embed/d28e1f50-2898-4747-a02d-01439edbc13c/mfCpMXpv7t.json"></iframe>',unsafe_allow_html=True)
-    else:
-        st.markdown('<iframe src="https://lottie.host/embed/d28e1f50-2898-4747-a02d-01439edbc13c/mfCpMXpv7t.json"></iframe>',unsafe_allow_html=True)
+        st.markdown('<iframe src="https://lottie.host/embed/c36eebb5-88ec-4a33-99d2-549cee0eea4d/uikYxqE3PQ.json"></iframe>',unsafe_allow_html=True)
 with col3:
-    curr2 = st.selectbox('Currency 2',['EUR','USD','GBP'])
+    curr2 = st.selectbox('Currency 2',['EUR','USD','GBP','INR'])
+if curr1=='USD':
+    if curr2=='EUR': one_two=0.91
+    elif curr2 == 'USD': one_two=1
+    elif curr2 == 'INR': one_two=83.42
+    elif curr2 == 'GBP': one_two=0.79
+if curr1=='EUR':
+    if curr2=='EUR': one_two=1
+    elif curr2 == 'USD': one_two=1.09
+    elif curr2 == 'INR': one_two=91.11
+    elif curr2 == 'GBP': one_two=0.87
+if curr1=='GBP':
+    if curr2=='EUR': one_two=1.15
+    elif curr2 == 'USD': one_two=1.26
+    elif curr2 == 'INR': one_two=105.21
+    elif curr2 == 'GBP': one_two=1
+if curr1=='INR':
+    if curr2=='EUR': one_two=0.011
+    elif curr2 == 'USD': one_two=0.012
+    elif curr2 == 'INR': one_two=1
+    elif curr2 == 'GBP': one_two=0.0095
+with col1:
+    amount=st.number_input(curr1)
+with col3:
+    converted=amount*one_two
+    st.success(converted)
 
+st.markdown('<style> body{text-align:center;} </style>',unsafe_allow_html=True)
